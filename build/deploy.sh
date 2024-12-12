682beb38495a94a4ad3d06fa8e157703050863dd
S. C. Arzehd <scarzehd@gmail.com>
2024-12-12 01:11:07 -0500

    Fixed the build script and changed version to 0.4.5

---
+++ build/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/env bash
+
+GODOT="$HOME/Documents/dev/godot/Godot_v4.3-stable_linux.x86_64"
+
+if [ ! -f $GODOT ]; then
+	echo "Godot executable not found. Nothing has been changed."
+	return 1
+fi
+
+read -p "This will delete all current builds. Are you sure you want to continue? [y/N] " -n 1 -r
+echo
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+	echo "Make backups and try again."
+	return 1
+fi
+
+rm windows/*
+rm linux/*
+rm macos/*
+rm -r web/*
+
+VERSION=$( cat ../project.godot | grep config/version | sed 's/config\/version="//' | sed 's/"//' )
+
+mkdir web/${VERSION}
+
+PROJECT_PATH=$( pwd )/../
+
+$GODOT --headless --path $PROJECT_PATH --export-release "Windows" build/windows/fnas-windows-${VERSION}.exe
+$GODOT --headless --path $PROJECT_PATH --export-release "Linux" build/linux/fnas-linux-${VERSION}.x86_64
+$GODOT --headless --path $PROJECT_PATH --export-release "macOS" build/macos/fnas-macos-${VERSION}.zip
+$GODOT --headless --path $PROJECT_PATH --export-release "Web" build/web/${VERSION}/index.html
+
+butler push windows/ scarzehd/five-nights-at-shithouse:windows --userversion $VERSION
+butler push linux/ scarzehd/five-nights-at-shithouse:linux --userversion $VERSION
+butler push web/${VERSION}/ scarzehd/five-nights-at-shithouse:web --userversion $VERSION
+butler push macos/fnas-macos-${VERSION}.zip scarzehd/five-nights-at-shithouse:macos --userversion $VERSION
