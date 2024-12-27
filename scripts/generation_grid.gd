4622b16696fa20cc7f52508b200ee2932a0e5992
S. C. Arzehd <scarzehd@gmail.com>
2024-12-27 18:45:27 -0500

    The world now generates 3 chunks at the start.

--- scripts/generation_grid.gd
+++ scripts/generation_grid.gd
@@ -29,8 +29,9 @@ var center = Vector2i(0, 0)
 
 #region Constants
 
-const CHUNK_WIDTH = 64
-const CHUNK_HEIGHT = 64
+const CHUNK_SIZE = 64
+
+const WORLD_SIZE = 3
 
 const ROOM_PADDING = 4
 
@@ -41,25 +42,33 @@ const MAX_ROOM_SIZE = 12
 #endregion
 
 func _ready():
-	var chunk_bounds := get_chunk_bounds()
-	for x in range(chunk_bounds.position.x, chunk_bounds.end.x + 1):
-		for y in range(chunk_bounds.position.y, chunk_bounds.end.y + 1):
-			cells[Vector2i(x, y)] = CellData.new(Vector2i(x, y))
+	# Center the world size on (0, 0)
+	var border_chunks = (WORLD_SIZE - 1) / 2
 	
-	generate()
+	# Initialize cells
+	for chunk_x in range(-border_chunks, border_chunks + 1):
+		for chunk_y in range(-border_chunks, border_chunks + 1):
+			var chunk_center = Vector2i(chunk_x, chunk_y) * CHUNK_SIZE
+			init_chunk(chunk_center)
+	
+	for chunk_x in range(-border_chunks, border_chunks + 1):
+		for chunk_y in range(-border_chunks, border_chunks + 1):
+			var chunk_center = Vector2i(chunk_x, chunk_y) * CHUNK_SIZE
+			generate(chunk_center)
 	
 	add_meshes()
 
-func generate():
-	gen_rooms()
+func generate(chunk_center:Vector2i):
+	gen_rooms(chunk_center)
+	gen_halls(chunk_center)
 
-func gen_rooms():
-	var chunk_bounds := get_chunk_bounds()
+func gen_rooms(chunk_center:Vector2i):
+	var chunk_bounds := get_chunk_bounds(chunk_center)
 	
 	var rooms:Array[Rect2i] = []
 	
 	for i in range(MAX_ROOMS):
-		var corner = random_point()
+		var corner = random_point_in_chunk(chunk_center)
 		var size = Vector2i()
 		size.x = randi_range(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
 		size.y = randi_range(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
@@ -90,10 +99,13 @@ func add_rooms(rooms:Array[Rect2i]):
 				cells[Vector2i(x, y)].type = CellType.ROOM
 
 func add_meshes():
-	for position in cells:
-		var cell = cells[position]
+	for cell_position in cells:
+		var cell = cells[cell_position]
 		if cell.type == CellType.ROOM:
-			set_cell_item(Vector3i(position.x, 0, position.y), 1)
+			set_cell_item(Vector3i(cell_position.x, 0, cell_position.y), 1)
+
+func gen_halls(chunk_center:Vector2i):
+	pass
 
 #region Utilities
 
@@ -110,15 +122,24 @@ func shrink_to_fit(rect:Rect2i, encloser:Rect2i) -> Rect2i:
 	
 
 
-func random_point() -> Vector2i:
+func random_point_in_chunk(chunk_center:Vector2i) -> Vector2i:
 	var point = Vector2i()
-	var chunk_bounds := get_chunk_bounds()
+	var chunk_bounds := get_chunk_bounds(chunk_center)
 	point.x = randi_range(chunk_bounds.position.x, chunk_bounds.end.x)
 	point.y = randi_range(chunk_bounds.position.y, chunk_bounds.end.y)
 	
 	return point
 
-func get_chunk_bounds() -> Rect2i:
-	return Rect2i(Vector2i(center.x - (CHUNK_WIDTH / 2), center.y - (CHUNK_HEIGHT / 2)), Vector2i(CHUNK_WIDTH, CHUNK_HEIGHT))
+func get_chunk_bounds(chunk_center:Vector2i) -> Rect2i:
+	return Rect2i(Vector2i(chunk_center.x - (CHUNK_SIZE / 2), chunk_center.y - (CHUNK_SIZE / 2)), Vector2i(CHUNK_SIZE, CHUNK_SIZE))
+
+#func get_world_bounds() -> Rect2i:
+	#pass
+
+func init_chunk(chunk_center:Vector2i):
+	var chunk_bounds := get_chunk_bounds(chunk_center)
+	for x in range(chunk_bounds.position.x, chunk_bounds.end.x + 1):
+		for y in range(chunk_bounds.position.y, chunk_bounds.end.y + 1):
+			cells[Vector2i(x, y)] = CellData.new(Vector2i(x, y))
 
 #endregion
