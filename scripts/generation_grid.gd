0af39173547005598eba7592816d5e2cf1a3b991
S. C. Arzehd <scarzehd@gmail.com>
2025-01-11 18:27:13 -0500

    Generation thread now properly uses semaphores and mutexes and the like Test setup for moving the world center

--- scripts/generation_grid.gd
+++ scripts/generation_grid.gd
@@ -27,6 +27,10 @@ var cells = {}
 var world_center = Vector2i(0, 0)
 
 var generation_thread:Thread
+var generation_mutex:Mutex
+var chunks_to_generate:Array[Vector2i] = []
+var generation_semaphore:Semaphore
+var stop_generation := false
 
 #endregion
 
@@ -57,13 +61,42 @@ const VENT_DEBUG_MESH = preload("res://scenes/debug/vent_debug_mesh.tscn")
 
 const DEBUG_GRID_CELL_SIZE = 1
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_right"):
+		generation_mutex.lock()
+		world_center.x += CHUNK_SIZE
+		generation_mutex.unlock()
+		
+		reset_outside_cells()
+		
+		var chunks := get_chunks_in_world()
+		
+		generation_mutex.lock()
+		var new_chunks = chunks.filter(func(chunk): return not cells.keys().has(chunk))
+		generation_mutex.unlock()
+		
+		print(new_chunks)
+		
+		queue_generate(new_chunks)
+
 #endregion
 
 func _ready():
 	generation_thread = Thread.new()
-	generation_thread.start(initialize)
+	generation_mutex = Mutex.new()
+	generation_semaphore = Semaphore.new()
+	
+	generation_thread.start(generate)
+	
+	queue_generate(get_chunks_in_world())
 
 func _exit_tree():
+	generation_mutex.lock()
+	stop_generation = true
+	generation_mutex.unlock()
+	
+	generation_semaphore.post()
+	
 	generation_thread.wait_to_finish()
 
 func initialize():
@@ -101,24 +134,86 @@ func initialize():
 	add_meshes.call_deferred()
 	print("Done!")
 
-func generate(chunk_center:Vector2i):
-	gen_rooms(chunk_center)
-	gen_halls(chunk_center)
+func queue_generate(chunk_centers:Array[Vector2i]):
+	generation_mutex.lock()
+	chunks_to_generate.append_array(chunk_centers)
+	generation_mutex.unlock()
+	
+	generation_semaphore.post()
+
+func generate():
+	while true:
+		generation_semaphore.wait()
+		
+		generation_mutex.lock()
+		var exit = stop_generation
+		generation_mutex.unlock()
+		
+		if exit:
+			break
+		
+		generation_mutex.lock()
+		var chunk_centers = chunks_to_generate
+		chunks_to_generate = []
+		generation_mutex.unlock()
+		
+		for chunk_center in chunk_centers:
+			init_chunk(chunk_center)
+		
+		for chunk_center in chunk_centers:
+			while true:
+				gen_rooms(chunk_center)
+				var success = gen_halls(chunk_center)
+				if not success:
+					print("Halls failed to generate in chunk " + str(chunk_center) + ". Resetting chunk.")
+					init_chunk(chunk_center)
+					continue
+				success = prune_chunk(chunk_center)
+				
+				if not success:
+					# Check leftwards and upwards chunks
+					var adjacent_chunk_centers = [Vector2i(chunk_center.x - CHUNK_SIZE, chunk_center.y), Vector2i(chunk_center.x, chunk_center.y - CHUNK_SIZE)]
+					
+					var world_bounds = get_world_bounds()
+					
+					var first_chunk = true
+					
+					for adjacent_chunk in adjacent_chunk_centers:
+						if world_bounds.has_point(adjacent_chunk):
+							first_chunk = false
+					
+					if first_chunk:
+						break
+					
+					init_chunk(chunk_center)
+					continue
+					
+				#if not success and chunk_y != -border_chunks and chunk_x != -border_chunks:
+					#init_chunk(chunk_center)
+					#continue
+				
+				break
+			
+		update_meshes.call_deferred()
 
 func add_meshes():
 	for cell_position in cells:
-		var cell = cells[cell_position]
+		var cell = get_cell_data(cell_position)
 		if cell.type == CellType.ROOM:
 			add_mesh(cell_position, ROOM_DEBUG_MESH)
 		if cell.type == CellType.HALL:
 			add_mesh(cell_position, HALL_DEBUG_MESH)
+
+func update_meshes():
+	clear_meshes()
+	add_meshes()
 
 func add_meshes_in_chunk(chunk_center:Vector2i):
 	var chunk_bounds = get_chunk_bounds(chunk_center)
 	for x in range(chunk_bounds.position.x, chunk_bounds.end.x):
 		for y in range(chunk_bounds.position.y, chunk_bounds.end.y):
 			var cell_pos = Vector2i(x, y)
-			var cell = cells[cell_pos]
+			var cell = get_cell_data(cell_pos)
 			if cell.type == CellType.ROOM:
 				add_mesh(cell_pos, ROOM_DEBUG_MESH)
 			if cell.type == CellType.HALL:
@@ -190,7 +285,7 @@ func gen_halls(chunk_center:Vector2i) -> bool:
 			if !get_world_bounds().has_point(new_cell_pos):
 				break
 			
-			var new_cell:CellData = cells[new_cell_pos]
+			var new_cell:CellData = get_cell_data(new_cell_pos)
 			if new_cell.type != CellType.EMPTY:
 				if new_cell.type == CellType.ROOM and get_cell_normal(new_cell_pos).length() == 1:
 					end_cell_pos = new_cell_pos
@@ -246,7 +341,7 @@ func get_hall_candidate(chunk_center:Vector2i) -> Vector2i:
 	var chunk_bounds = get_chunk_bounds(chunk_center)
 	
 	var candidates = bounds_to_array(chunk_bounds).filter(func(cell):
-		return get_cell_normal(cell).length() == 1 and cells[cell].type == CellType.ROOM
+		return get_cell_normal(cell).length() == 1 and get_cell_data(cell).type == CellType.ROOM
 	)
 	
 	if candidates.size() == 0:
@@ -270,11 +365,26 @@ func get_hall_candidate(chunk_center:Vector2i) -> Vector2i:
 
 #region Utilities
 
+func get_chunks_in_world() -> Array[Vector2i]:
+	var chunks:Array[Vector2i] = []
+	
+	var border_chunks = (WORLD_SIZE - 1) / 2
+	
+	generation_mutex.lock()
+	var center = world_center
+	generation_mutex.unlock()
+	
+	for chunk_x in range(-border_chunks, border_chunks + 1):
+		for chunk_y in range(-border_chunks, border_chunks + 1):
+			chunks.append(Vector2i((chunk_x * CHUNK_SIZE) + center.x, (chunk_y * CHUNK_SIZE) + center.y))
+	
+	return chunks
+
 func set_cell_type_from_rects(rects:Array[Rect2i], cell_type:CellType):
 	for rect in rects:
 		for x in range(rect.position.x, rect.end.x):
 			for y in range(rect.position.y, rect.end.y):
-				cells[Vector2i(x, y)].type = cell_type
+				get_cell_data(Vector2i(x, y)).type = cell_type
 
 func shrink_to_fit(rect:Rect2i, encloser:Rect2i) -> Rect2i:
 	var new_rect = Rect2i(rect)
@@ -300,17 +410,20 @@ func get_chunk_bounds(chunk_center:Vector2i) -> Rect2i:
 	return Rect2i(Vector2i(chunk_center.x - (CHUNK_SIZE / 2), chunk_center.y - (CHUNK_SIZE / 2)), Vector2i(CHUNK_SIZE, CHUNK_SIZE))
 
 func get_world_bounds() -> Rect2i:
-	return get_chunk_bounds(world_center).grow(CHUNK_SIZE * (WORLD_SIZE - 1) / 2)
+	generation_mutex.lock()
+	var center = world_center
+	generation_mutex.unlock()
+	return get_chunk_bounds(center).grow(CHUNK_SIZE * (WORLD_SIZE - 1) / 2)
 
 func init_chunk(chunk_center:Vector2i):
 	var chunk_bounds := get_chunk_bounds(chunk_center)
 	for x in range(chunk_bounds.position.x, chunk_bounds.end.x + 1):
 		for y in range(chunk_bounds.position.y, chunk_bounds.end.y + 1):
-			cells[Vector2i(x, y)] = CellData.new(Vector2i(x, y))
+			set_cell_data(CellData.new(Vector2i(x, y))) 
 
 func get_cell_normal(cell_position:Vector2i) -> Vector2i:
 	var normal = Vector2i.ZERO
-	var cell:CellData = cells[cell_position]
+	var cell:CellData = get_cell_data(cell_position)
 	
 	if cell.type == CellType.EMPTY:
 		return normal
@@ -319,7 +432,7 @@ func get_cell_normal(cell_position:Vector2i) -> Vector2i:
 		var neighbor_position = Vector2i(cell_position.x + x, cell_position.y)
 		if !get_world_bounds().has_point(neighbor_position):
 			continue
-		var neighbor:CellData = cells[neighbor_position]
+		var neighbor:CellData = get_cell_data(neighbor_position)
 		if neighbor.type == CellType.EMPTY:
 			normal.x = x
 	
@@ -327,7 +440,7 @@ func get_cell_normal(cell_position:Vector2i) -> Vector2i:
 		var neighbor_position = Vector2i(cell_position.x, cell_position.y + y)
 		if !get_world_bounds().has_point(neighbor_position):
 			continue
-		var neighbor:CellData = cells[neighbor_position]
+		var neighbor:CellData = get_cell_data(neighbor_position)
 		if neighbor.type == CellType.EMPTY:
 			normal.y = y
 	
@@ -358,6 +471,31 @@ func bounds_to_array(bounds:Rect2i) -> Array[Vector2i]:
 			array.append(Vector2i(x, y))
 	
 	return array
+
+func get_cell_data(cell_pos:Vector2i) -> CellData:
+	generation_mutex.lock()
+	var cell_data = cells[cell_pos]
+	generation_mutex.unlock()
+	
+	return cell_data
+
+func set_cell_data(cell_data:CellData):
+	generation_mutex.lock()
+	cells[cell_data.position] = cell_data
+	generation_mutex.unlock()
+
+func reset_outside_cells():
+	var world_bounds = get_world_bounds()
+	
+	generation_mutex.lock()
+	var all_cells = cells.keys()
+	generation_mutex.unlock()
+	
+	var outside_cells = all_cells.filter(func(cell): return not world_bounds.has_point(cell as Vector2i))
+	
+	for cell in outside_cells:
+		set_cell_data(CellData.new(Vector2i(cell)))
+
 #endregion
 
 #region Verification
@@ -377,7 +515,7 @@ func verify_chunk(chunk_center:Vector2i, reference_chunk_center:Vector2i) -> boo
 			Vector2i.DOWN:
 				return cell.y > chunk_center.y
 		return false # Just in case
-	).filter(func(cell): return cells[cell].type == CellType.HALL)
+	).filter(func(cell): return get_cell_data(cell).type == CellType.HALL)
 	
 	return connecting_cells.size() > 0
 	
@@ -409,7 +547,7 @@ func prune_chunk(chunk_center:Vector2i) -> bool:
 	
 	var boundary_cells := get_cells_on_boundary(chunk_bounds)
 	
-	var starting_point_candidates = boundary_cells.filter(func(cell): return cells[cell].type == CellType.HALL)
+	var starting_point_candidates = boundary_cells.filter(func(cell): return get_cell_data(cell).type == CellType.HALL)
 	
 	if starting_point_candidates.size() == 0:
 		print("Chunk " + str(chunk_center) + " has no connections.")
@@ -427,7 +565,7 @@ func prune_chunk(chunk_center:Vector2i) -> bool:
 	
 	while frontier.size() > 0:
 		working_point = frontier[0]
-		if cells[working_point].type != CellType.EMPTY:
+		if get_cell_data(working_point).type != CellType.EMPTY:
 			if not flooded_list.has(working_point):
 				flooded_list.append(working_point)
 				if world_bounds.has_point(working_point + Vector2i.RIGHT):
@@ -444,7 +582,7 @@ func prune_chunk(chunk_center:Vector2i) -> bool:
 	var cells_to_prune = bounds_to_array(chunk_bounds).filter(func(cell): return not flooded_list.has(cell))
 	
 	for cell in cells_to_prune:
-		cells[cell].type = CellType.EMPTY
+		get_cell_data(cell).type = CellType.EMPTY
 	
 	return true
 
