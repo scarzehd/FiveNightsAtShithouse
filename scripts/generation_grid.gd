6fec1763683f44af4f8c0ce697ebcdbbe423aa68
S. C. Arzehd <scarzehd@gmail.com>
2025-01-11 06:49:15 -0500

    Unreachable rooms will now be pruned and unreachable chunks will be regenerated. Chunk generation moved to a separate thread.

--- scripts/generation_grid.gd
+++ scripts/generation_grid.gd
@@ -26,21 +26,23 @@ class CellData:
 var cells = {}
 var world_center = Vector2i(0, 0)
 
+var generation_thread:Thread
+
 #endregion
 
 #region Constants
 
-const CHUNK_SIZE = 64
+const CHUNK_SIZE = 32
 
-const WORLD_SIZE = 3
+const WORLD_SIZE = 5
 
 const ROOM_PADDING = 4
 
-const MAX_ROOMS = 20
+const MAX_ROOMS = 40
 const MIN_ROOM_SIZE = 6
-const MAX_ROOM_SIZE = 14
+const MAX_ROOM_SIZE = 20
 
-const NUM_HALLS = 40
+const NUM_HALLS = 50
 const MAX_HALL_SIZE = 50
 const HALL_WIDTH = 3
 const HALL_PADDING = 4
@@ -58,7 +60,11 @@ const DEBUG_GRID_CELL_SIZE = 1
 #endregion
 
 func _ready():
-	initialize()
+	generation_thread = Thread.new()
+	generation_thread.start(initialize)
+
+func _exit_tree():
+	generation_thread.wait_to_finish()
 
 func initialize():
 	# Center the world size on (0, 0)
@@ -70,27 +76,30 @@ func initialize():
 			var chunk_center = Vector2i(chunk_x, chunk_y) * CHUNK_SIZE
 			init_chunk(chunk_center)
 	
-	var previous_chunk_center = null
+	# Cache this so we don't call it a bajillion times
+	var world_bounds := get_world_bounds()
 	
 	# Generate rooms first, then halls.
-	for chunk_x in range(-border_chunks, border_chunks + 1):
-		for chunk_y in range(-border_chunks, border_chunks + 1):
-			var chunk_center = Vector2i(chunk_x, chunk_y) * CHUNK_SIZE
-			while true:
-				gen_rooms(chunk_center)
-				gen_halls(chunk_center)
-				if previous_chunk_center != null and verify_chunk(chunk_center, previous_chunk_center):
-					init_chunk(chunk_center)
-					continue
-				previous_chunk_center = chunk_center
-				break
-
-	for chunk_x in range(-border_chunks, border_chunks + 1):
-		for chunk_y in range(-border_chunks, border_chunks + 1):
+	for chunk_y in range(-border_chunks, border_chunks + 1):
+		for chunk_x in range(-border_chunks, border_chunks + 1):
 			var chunk_center = Vector2i(chunk_x, chunk_y) * CHUNK_SIZE
 			
+			while true:
+				gen_rooms(chunk_center)
+				var success = gen_halls(chunk_center)
+				if not success:
+					print("Halls failed to generate in chunk " + str(chunk_center) + ". Resetting chunk.")
+					init_chunk(chunk_center)
+					continue
+				success = prune_chunk(chunk_center)
+				if not success and chunk_y != -border_chunks and chunk_x != -border_chunks:
+					init_chunk(chunk_center)
+					continue
+				
+				break
 	
-	add_meshes()
+	add_meshes.call_deferred()
+	print("Done!")
 
 func generate(chunk_center:Vector2i):
 	gen_rooms(chunk_center)
@@ -104,9 +113,25 @@ func add_meshes():
 		if cell.type == CellType.HALL:
 			add_mesh(cell_position, HALL_DEBUG_MESH)
 
-func add_mesh(position:Vector2i, scene:PackedScene):
+func add_meshes_in_chunk(chunk_center:Vector2i):
+	var chunk_bounds = get_chunk_bounds(chunk_center)
+	for x in range(chunk_bounds.position.x, chunk_bounds.end.x):
+		for y in range(chunk_bounds.position.y, chunk_bounds.end.y):
+			var cell_pos = Vector2i(x, y)
+			var cell = cells[cell_pos]
+			if cell.type == CellType.ROOM:
+				add_mesh(cell_pos, ROOM_DEBUG_MESH)
+			if cell.type == CellType.HALL:
+				add_mesh(cell_pos, HALL_DEBUG_MESH)
+
+func clear_meshes():
+	for child in get_children():
+		if child is MeshInstance3D:
+			child.queue_free()
+
+func add_mesh(spawn_pos:Vector2i, scene:PackedScene):
 	var mesh:MeshInstance3D = scene.instantiate()
-	mesh.position = Vector3(position.x * DEBUG_GRID_CELL_SIZE, 0, position.y * DEBUG_GRID_CELL_SIZE)
+	mesh.position = Vector3(spawn_pos.x * DEBUG_GRID_CELL_SIZE, 0, spawn_pos.y * DEBUG_GRID_CELL_SIZE)
 	add_child(mesh)
 
 #region Room Generation
@@ -146,7 +171,7 @@ func gen_rooms(chunk_center:Vector2i):
 
 #region Hall Generation
 
-func gen_halls(chunk_center:Vector2i):
+func gen_halls(chunk_center:Vector2i) -> bool:
 	var hall_rects:Array[Rect2i] = []
 	
 	for _i in range(NUM_HALLS):
@@ -189,7 +214,12 @@ func gen_halls(chunk_center:Vector2i):
 		hall_rects.append(hall_rect)
 		#print(hall_rect)
 	
+	if hall_rects.size() == 0:
+		return false
+	
 	set_cell_type_from_rects(hall_rects, CellType.HALL)
+	
+	return true
 
 func create_hall_rect(start_cell_pos:Vector2i, end_cell_pos:Vector2i) -> Rect2i:
 	var hall_width_centered = (HALL_WIDTH - 1) / 2
@@ -213,16 +243,28 @@ func create_hall_rect(start_cell_pos:Vector2i, end_cell_pos:Vector2i) -> Rect2i:
 	return hall_rect
 
 func get_hall_candidate(chunk_center:Vector2i) -> Vector2i:
-	var cell_position := random_point_in_chunk(chunk_center)
-	var cell_normal := get_cell_normal(cell_position)
-	while true:
-		cell_position = random_point_in_chunk(chunk_center)
-		cell_normal = get_cell_normal(cell_position)
-		
-		if cell_normal.length() == 1 and cells[cell_position].type == CellType.ROOM:
-			break
+	var chunk_bounds = get_chunk_bounds(chunk_center)
 	
-	return cell_position
+	var candidates = bounds_to_array(chunk_bounds).filter(func(cell):
+		return get_cell_normal(cell).length() == 1 and cells[cell].type == CellType.ROOM
+	)
+	
+	if candidates.size() == 0:
+		print("No hall candidates in chunk " + str(chunk_center) + ".")
+		return Vector2i.ZERO
+	
+	return candidates.pick_random()
+	
+	#var cell_position := random_point_in_chunk(chunk_center)
+	#var cell_normal := get_cell_normal(cell_position)
+	#while true:
+		#cell_position = random_point_in_chunk(chunk_center)
+		#cell_normal = get_cell_normal(cell_position)
+		#
+		#if cell_normal.length() == 1 and cells[cell_position].type == CellType.ROOM:
+			#break
+	#
+	#return cell_position
 
 #endregion
 
@@ -252,11 +294,7 @@ func random_point_in_world() -> Vector2i:
 	return random_point_in_bounds(get_world_bounds())
 
 func random_point_in_bounds(bounds:Rect2i) -> Vector2i:
-	var point = Vector2i()
-	point.x = randi_range(bounds.position.x, bounds.end.x)
-	point.y = randi_range(bounds.position.y, bounds.end.y)
-	
-	return point
+	return bounds_to_array(bounds).pick_random()
 
 func get_chunk_bounds(chunk_center:Vector2i) -> Rect2i:
 	return Rect2i(Vector2i(chunk_center.x - (CHUNK_SIZE / 2), chunk_center.y - (CHUNK_SIZE / 2)), Vector2i(CHUNK_SIZE, CHUNK_SIZE))
@@ -298,34 +336,116 @@ func get_cell_normal(cell_position:Vector2i) -> Vector2i:
 	
 	return normal
 
-func verify_chunk(chunk_center:Vector2i, reference_chunk_center:Vector2i) -> bool:
-	var chunk_bounds := get_chunk_bounds(chunk_center)
-	var reference_direction = (chunk_center - reference_chunk_center).sign()
+func cell_is_on_boundary(bounds:Rect2i, cell:Vector2i) -> bool:
+	if cell.x == bounds.position.x or cell.x == bounds.end.x:
+		return true
 	
-	var x = []
-	var y = []
-	
-	match reference_direction:
-		Vector2i.RIGHT:
-			x = [chunk_bounds.end.x]
-			y = range(chunk_bounds.position.y, chunk_bounds.end.y)
-		Vector2i.LEFT:
-			x = [chunk_bounds.position.x]
-			y = range(chunk_bounds.position.y, chunk_bounds.end.y)
-		Vector2i.UP:
-			x = range(chunk_bounds.position.x, chunk_bounds.end.x)
-			y = [chunk_bounds.position.y]
-		Vector2i.DOWN:
-			x = range(chunk_bounds.position.x, chunk_bounds.end.x)
-			y = [chunk_bounds.end.y]
-	
-	
-	for x_pos in x:
-		for y_pos in y:
-			print(Vector2i(x_pos, y_pos))
-			if cells[Vector2i(x_pos, y_pos)].type != CellType.EMPTY:
-				return true
+	if cell.y == bounds.position.y or cell.y == bounds.end.y:
+		return true
 	
 	return false
+
+func get_cells_on_boundary(bounds:Rect2i) -> Array[Vector2i]:
+	var all_cells := bounds_to_array(bounds)
+	var filtered_cells = all_cells.filter(func(cell): return cell_is_on_boundary(bounds, cell))
+	return filtered_cells
+
+func bounds_to_array(bounds:Rect2i) -> Array[Vector2i]:
+	var array:Array[Vector2i] = []
+	
+	for x  in range(bounds.position.x, bounds.end.x):
+		for y in range(bounds.position.y, bounds.end.y):
+			array.append(Vector2i(x, y))
+	
+	return array
+#endregion
+
+#region Verification
+
+func verify_chunk(chunk_center:Vector2i, reference_chunk_center:Vector2i) -> bool:
+	var chunk_bounds := get_chunk_bounds(chunk_center)
+	var reference_direction = (reference_chunk_center - chunk_center).sign()
+	
+	var connecting_cells = get_cells_on_boundary(chunk_bounds).filter(func(cell):
+		match reference_direction:
+			Vector2i.RIGHT:
+				return cell.x > chunk_center.x
+			Vector2i.LEFT:
+				return cell.x < chunk_center.x
+			Vector2i.UP:
+				return cell.y < chunk_center.y
+			Vector2i.DOWN:
+				return cell.y > chunk_center.y
+		return false # Just in case
+	).filter(func(cell): return cells[cell].type == CellType.HALL)
+	
+	return connecting_cells.size() > 0
+	
+	#var x = []
+	#var y = []
+	#
+	#match reference_direction:
+		#Vector2i.RIGHT:
+			#x = [chunk_bounds.end.x]
+			#y = range(chunk_bounds.position.y, chunk_bounds.end.y)
+		#Vector2i.LEFT:
+			#x = [chunk_bounds.position.x]
+			#y = range(chunk_bounds.position.y, chunk_bounds.end.y)
+		#Vector2i.UP:
+			#x = range(chunk_bounds.position.x, chunk_bounds.end.x)
+			#y = [chunk_bounds.position.y]
+		#Vector2i.DOWN:
+			#x = range(chunk_bounds.position.x, chunk_bounds.end.x)
+			#y = [chunk_bounds.end.y]
+	#
+	#
+	#for x_pos in x:
+		#for y_pos in y:
+			#if cells[Vector2i(x_pos, y_pos)].type != CellType.EMPTY:
+				#return true
+
+func prune_chunk(chunk_center:Vector2i) -> bool:
+	var chunk_bounds = get_chunk_bounds(chunk_center)
+	
+	var boundary_cells := get_cells_on_boundary(chunk_bounds)
+	
+	var starting_point_candidates = boundary_cells.filter(func(cell): return cells[cell].type == CellType.HALL)
+	
+	if starting_point_candidates.size() == 0:
+		print("Chunk " + str(chunk_center) + " has no connections.")
+		return false
+		#starting_point_candidates = bounds_to_array(chunk_bounds).filter(func(cell): return cells[cell].type == CellType.HALL)
+	
+	var starting_point:Vector2i = starting_point_candidates.pick_random()
+	
+	var frontier:Array[Vector2i] = [starting_point]
+	var flooded_list:Array[Vector2i] = []
+	
+	var working_point = starting_point
+	
+	var world_bounds = get_world_bounds()
+	
+	while frontier.size() > 0:
+		working_point = frontier[0]
+		if cells[working_point].type != CellType.EMPTY:
+			if not flooded_list.has(working_point):
+				flooded_list.append(working_point)
+				if world_bounds.has_point(working_point + Vector2i.RIGHT):
+					frontier.append(working_point + Vector2i.RIGHT)
+				if world_bounds.has_point(working_point + Vector2i.LEFT):
+					frontier.append(working_point + Vector2i.LEFT)
+				if world_bounds.has_point(working_point + Vector2i.UP):
+					frontier.append(working_point + Vector2i.UP)
+				if world_bounds.has_point(working_point + Vector2i.DOWN):
+					frontier.append(working_point + Vector2i.DOWN)
+		
+		frontier.remove_at(0)
+		
+	var cells_to_prune = bounds_to_array(chunk_bounds).filter(func(cell): return not flooded_list.has(cell))
+	
+	for cell in cells_to_prune:
+		cells[cell].type = CellType.EMPTY
+	
+	return true
 
 #endregion
